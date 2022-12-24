8407fa021e5e5e3d48e2cd9bb15c772761e45e8c
anna <annagridchina@mail.ru>
2022-12-24 20:58:59 +0700

    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Start");
+int N = 3; 
+
+    for (int i = 1; i <= N; i++)
+    {
+    Console.WriteLine($"{i * i * i}, ");
+    }
+Console.WriteLine("END");
